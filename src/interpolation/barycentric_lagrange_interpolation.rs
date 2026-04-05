93bc9aef9da5c200a0f2d298bd7b093420997271
houru <houruxuan01@gmail.com>
2026-04-05 20:24:23 +1000

    (feat) add more tests for lagrange interpolation

--- src/interpolation/barycentric_lagrange_interpolation.rs
+++ src/interpolation/barycentric_lagrange_interpolation.rs
@@ -68,9 +68,44 @@ mod tests {
     }
 
     #[test]
+    fn test_interpolation_two_point() {
+        let xs = vec![1.0, 2.0];
+        let ys = vec![3.0, 5.0];
+        let poly1 = barycentric_lagrange_interpolation(xs, ys);
+        let expected_fn = |x: f64| 2.0*x + 1.0;
+        for i in 0..5 {
+            assert!((poly1.eval(i as f64).unwrap() - expected_fn(i as f64)).abs() < 1e-6);
+        }
+    }
+
+    #[test]
     fn test_interpolation_three_point() {
         let xs = vec![0.0, 2.0, 3.0];
         let ys = vec![1.0, 2.0, 4.0];
+        let poly2 = barycentric_lagrange_interpolation(xs, ys);
+        let poly2_y_values = vec!(1.0, 1.0, 2.0, 4.0, 7.0);
+        for i in 0..5 {
+            assert_eq!((poly2.eval(i as f64).unwrap() - poly2_y_values[i]).abs() < 1e-6, true);
+        }
+    }
+
+    #[test]
+    fn test_interpolation_five_point() {
+        let xs = vec![0.0, 1.0, 2.0, 3.0, 4.0];
+        let ys = vec![1.0, -1.0, 2.0, 4.0, 3.0];
+        let poly2 = barycentric_lagrange_interpolation(xs, ys);
+        let poly2_y_values = vec!(1.0, -1.0, 2.0, 4.0, 3.0);
+        for i in 0..5 {
+            assert_eq!((poly2.eval(i as f64).unwrap() - poly2_y_values[i]).abs() < 1e-6, true);
+        }
+        assert_eq!((poly2.eval(-1.0).unwrap() - 18.0).abs() < 1e-6, true);
+        assert_eq!((poly2.eval(6.0).unwrap() - 4.0).abs() < 1e-6, true);
+    }
+
+    #[test]
+    fn test_out_of_order_points() {
+        let xs = vec![3.0, 2.0, 0.0];
+        let ys = vec![4.0, 2.0, 1.0];
         let poly2 = barycentric_lagrange_interpolation(xs, ys);
         let poly2_y_values = vec!(1.0, 1.0, 2.0, 4.0, 7.0);
         for i in 0..5 {
