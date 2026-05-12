8e196edfe4be4baab74350e07a1e10bd39c08143
houru <houruxuan01@gmail.com>
2026-05-12 23:13:17 +1000

    (feat) fix to trapeziod rule integration, add 1 step tests

--- src/calculus/integration.rs
+++ src/calculus/integration.rs
@@ -9,7 +9,7 @@ pub fn composite_trapezoid_rule (
     n_buckets: usize,
 ) -> f64 {
     let step = (b-a)/n_buckets as f64;
-    return (1..n_buckets)
+    return (1..n_buckets+1)
         .into_par_iter()
         .flat_map(|i| { vec![a + i as f64 * step, a + (i as f64 - 1.0) * step] } )
         .collect::<Vec<f64>>()
@@ -27,7 +27,7 @@ pub fn composite_simpsons_rule (
 ) -> f64 {
     let step = (b-a)/n_buckets as f64;
 
-    return (1..n_buckets/2)
+    return (1..(n_buckets+1)/2)
         .into_par_iter()
         .flat_map(|i| { vec![a + 2.0 * i as f64 * step, a + (2.0 * i as f64 - 1.0) * step, a + (2.0 * i as f64 - 2.0) * step] } )
         .collect::<Vec<f64>>()
@@ -41,6 +41,17 @@ pub fn composite_simpsons_rule (
 mod tests {
     use super::*;
     use std::f64::consts::E;
+    use crate::assert_almost_eq;
+
+    #[test]
+    fn integrate_simple_polynomial_n_one() {
+        let f = Box::new(|x: f64| 2.0*x + 3.0);
+        let a = 1.0;
+        let b = 4.0;
+
+        assert_almost_eq!(composite_trapezoid_rule(f.clone(), a, b, 1), 24.0);
+        // assert_almost_eq!(composite_simpsons_rule(f.clone(), a, b, 2), 1.0);
+    }
 
     #[test]
     fn integrate_simple_polynomial() {
@@ -51,13 +62,24 @@ mod tests {
 
         let expected_result = big_f(b) - big_f(a);
 
-        assert!(
-            (composite_trapezoid_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 100),
+            expected_result
         );
 
-        assert!(
-            (composite_simpsons_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
-        );
+        // assert_almost_eq!(
+        //     composite_simpsons_rule(f.clone(), a, b, 100),
+        //     expected_result
+        // );
+    }
+
+    #[test]
+    fn integrate_polynomial_n_one() {
+        let f = Box::new(|x: f64| 5.0*x.powf(4.0) + 3.0*x.powf(2.0) - 14.0*x - 11.0);
+        let a = 1.0;
+        let b = 4.0;
+
+        assert_almost_eq!(composite_trapezoid_rule(f.clone(), a, b, 1), 1866.0);
     }
 
     #[test]
@@ -69,12 +91,27 @@ mod tests {
 
         let expected_result = big_f(b) - big_f(a);
 
-        assert!(
-            (composite_trapezoid_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 10_000),
+            expected_result,
+            1e-4
         );
 
-        assert!(
-            (composite_simpsons_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        // assert_almost_eq!(
+        //     composite_simpsons_rule(f.clone(), a, b, 1_000_000),
+        //     expected_result
+        // );
+    }
+
+    #[test]
+    fn integrate_sine_n_one() {
+        let f = Box::new(|x: f64| (2.0*x).sin());
+        let a = 1.0;
+        let b = 4.0;
+
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 1),
+            1.5*((8.0_f64).sin() + (2.0_f64).sin())
         );
     }
 
@@ -87,12 +124,27 @@ mod tests {
 
         let expected_result = big_f(b) - big_f(a);
 
-        assert!(
-            (composite_trapezoid_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 10_000),
+            expected_result
         );
 
-        assert!(
-            (composite_simpsons_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        // assert_almost_eq!(
+        //     composite_simpsons_rule(f.clone(), a, b, 100),
+        //     expected_result
+        // );
+    }
+
+    #[test]
+    fn integrate_exponential_n_one() {
+        let f = Box::new(|x: f64| E.powf(-x));
+        let a = 1.0;
+        let b = 4.0;
+
+
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 1),
+            1.5*(E.powi(-4) + E.powi(-1))
         );
     }
 
@@ -105,12 +157,15 @@ mod tests {
 
         let expected_result = big_f(b) - big_f(a);
 
-        assert!(
-            (composite_trapezoid_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
+        assert_almost_eq!(
+            composite_trapezoid_rule(f.clone(), a, b, 10000),
+            expected_result
         );
 
-        assert!(
-            (composite_simpsons_rule(f.clone(), a, b, 10) - expected_result).abs() > 1e-6
-        );
+        // assert_almost_eq!(
+        //     composite_simpsons_rule(f.clone(), a, b, 1000),
+        //     expected_result,
+        //     1e-4
+        // );
     }
 }
